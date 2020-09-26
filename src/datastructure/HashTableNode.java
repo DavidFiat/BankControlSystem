cfe8d53857ab634b07436978630f442995c29df7
David <davidfiat24@gmail.com>
2020-09-26 00:24:18 -0500

    Hash Body

---
+++ src/datastructure/HashTableNode.java
@@ -0,0 +1,48 @@
+package datastructure;
+
+public class HashTableNode<K, V> {
+
+	private K key;
+	private V value;
+
+	private HashTableNode<K, V> next;
+	private HashTableNode<K, V> prev;
+
+	public HashTableNode(K key, V value) {
+		this.setKey(key);
+		this.setValue(value);
+	}
+
+	public K getKey() {
+		return key;
+	}
+
+	public void setKey(K key) {
+		this.key = key;
+	}
+
+	public V getValue() {
+		return value;
+	}
+
+	public void setValue(V value) {
+		this.value = value;
+	}
+
+	public HashTableNode<K, V> getNext() {
+		return next;
+	}
+
+	public void setNext(HashTableNode<K, V> next) {
+		this.next = next;
+	}
+
+	public HashTableNode<K, V> getPrev() {
+		return prev;
+	}
+
+	public void setPrev(HashTableNode<K, V> prev) {
+		this.prev = prev;
+	}
+
+}
